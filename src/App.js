8f967cdd467522eaf8a47d266324a87f05ffce69
Huzaifah <mhuzaifah101@gmail.com>
2024-07-22 09:14:42 +0100

    first change

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Huzaifah</h1>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit and save to reload.
         </p>
         <a
           className="App-link"
